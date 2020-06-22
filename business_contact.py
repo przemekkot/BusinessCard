f4ca38a6006ad81a9556f8f173de59529a765563
przemekkot <65823229+przemekkot@users.noreply.github.com>
2020-06-22 20:22:59 +0200

    Fix code
    create_contacts function returns now list of instances, not a list of strings

--- business_contact.py
+++ business_contact.py
@@ -33,7 +33,7 @@ class BusinessContact(BaseContact):
         return f"{self.company} {self.position}, {self.business_phone_number}"
 
     def __repr__(self):
-        return f"BusinessContact(company={self.company} position={self.position}, business_phone_number={self.business_phone_number})"
+        return f"BusinessContact(first_name={self.first_name} last_name={self.last_name}, email_address={self.email_address}, company={self.company} position={self.position}, business_phone_number={self.business_phone_number})"
 
     def contact_job(self):
         return f"Wybieram numer {self.business_phone_number} i dzwonię do {self.first_name} {self.last_name}, stanowisko: {self.position} z firmy {self.company}"
@@ -41,7 +41,9 @@ class BusinessContact(BaseContact):
     @property
     def label_length(self):
         return sum([len(self.first_name), len(self.last_name), 1])
-person = BusinessContact(first_name=fake.first_name(), last_name=fake.last_name(), phone_number=fake.phone_number(), email_address=fake.email(), company=fake.company(), position=fake.job(), business_phone_number=fake.phone_number())           
+
+person_priv = BaseContact(first_name=fake.first_name(), last_name=fake.last_name(), phone_number=fake.phone_number(), email_address=fake.email())           
+person_business = BusinessContact(first_name=fake.first_name(), last_name=fake.last_name(), phone_number=fake.phone_number(), email_address=fake.email(), company=fake.company(), position=fake.job(), business_phone_number=fake.phone_number())
 
 # variables
 type = ("private", "business")
@@ -52,15 +54,18 @@ list_of_contacts = []
 def create_contacts(type, how_many):
     if type == "private":
         for i in range(how_many):
-            contact = f"{fake.first_name()}, {fake.last_name()}, {fake.phone_number()}, {fake.email()}"
-            list_of_contacts.append(contact)
+            #contact = f"{fake.first_name()}, {fake.last_name()}, {fake.phone_number()}, {fake.email()}"
+            list_of_contacts.append(person_priv)
     elif type == "business":
         for i in range(how_many):
-            contact = f"{fake.first_name()}, {fake.last_name()}, {fake.phone_number()}, {fake.email()}, {fake.company()}, {fake.job()}"
-            list_of_contacts.append(contact)
+            #contact = f"{fake.first_name()}, {fake.last_name()}, {fake.phone_number()}, {fake.email()}, {fake.company()}, {fake.job()}"
+            list_of_contacts.append(person_business)
     else:
         exit(1)
     return list_of_contacts   
-    
 
-print(create_contacts("business", 4))
+#print(person)
+#print(person.contact_priv())
+#print(person.contact_job())
+
+print(create_contacts("business", 3))
